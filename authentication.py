fd1f4b730fb02eb3445cb9c8c1f4dd0bf40f1189
saljoudi <50462909+saljoudi@users.noreply.github.com>
2025-03-10 02:12:37 -0400

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -1,42 +1,15 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, logout_user, login_required
-from database import db, User
+from flask_login import UserMixin
+from database import get_user
 
-auth = Blueprint('auth', __name__)
+class User(UserMixin):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
 
-@auth.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        user = User.query.filter_by(email=email).first()
-        if user and check_password_hash(user.password, password):
-            login_user(user)
-            return redirect(url_for('main.profile'))
-        else:
-            flash('Login failed. Check your email and password.')
-    return render_template('login.html')
-
-@auth.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        username = request.form.get('username')
-        password = request.form.get('password')
-        user = User.query.filter_by(email=email).first()
+    @staticmethod
+    def get(user_id):
+        user = get_user(user_id)
         if user:
-            flash('Email address already exists')
-        else:
-            new_user = User(email=email, username=username, password=generate_password_hash(password, method='sha256'))
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user)
-            return redirect(url_for('main.profile'))
-    return render_template('signup.html')
-
-@auth.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('auth.login'))
+            return User(id=user['id'], username=user['username'], password=user['password'])
+        return None
